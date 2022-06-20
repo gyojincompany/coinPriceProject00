d12b97ffb0d0d1b544ca579e0ab041d3ef700275
gyojincompany <luckyred@gmail.com>
2022-06-20 13:21:46 +0900

    v1.0 완성

--- tickerTest.py
+++ tickerTest.py
@@ -1,13 +1,11 @@
-import requests
 import pyupbit
 
-url = "https://api.upbit.com/v1/market/all?isDetails=false"
-
-headers = {"Accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
-
-ticker_list = pyupbit.get_tickers()
+ticker_list = pyupbit.get_tickers(fiat="KRW")
 print(ticker_list)
+
+coin_list = []
+for ticker in ticker_list:
+    #print(ticker[4:10])
+    coin_list.append(ticker[4:10])
+
+print(coin_list)
