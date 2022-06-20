eae6620750038496c4561e718a69a4b57ca4e223
gyojincompany <luckyred@gmail.com>
2022-06-20 09:59:29 +0900

    coinViewThread run함수 내용 추가

--- coin_price_project_v.0.5.py
+++ coin_price_project_v.0.5.py
@@ -1,4 +1,7 @@
 import sys
+import time
+
+import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
@@ -7,6 +10,7 @@ from PyQt5.QtCore import *
 form_class = uic.loadUiType("ui/coinPriceUi.ui")[0]
 
 class CoinViewThread(QThread):
+    # 시그널 함수 정의
     coinDataSent = pyqtSignal(float, float, float, float, float, float, float, float)
 
     def __init__(self):
@@ -15,7 +19,36 @@ class CoinViewThread(QThread):
         self.alive = True
 
     def run(self):
+        # 업비트 정보 api 호출 반복
+        while self.alive:
+            url = "https://api.upbit.com/v1/ticker"
+            param = {"markets": "KRW-BTC"}
+            response = requests.get(url, params=param)
+            upbitResult = response.json()
 
+            trade_price = upbitResult[0]['trade_price']  # 현재가
+            acc_trade_volume_24h = upbitResult[0]['acc_trade_volume_24h']  # 24시간 거래량
+            acc_trade_price_24h = upbitResult[0]['acc_trade_price_24h']  # 24시간 누적 거래대금
+            high_price = upbitResult[0]['high_price']  # 고가
+            low_price = upbitResult[0]['low_price']  # 저가
+            prev_closing_price = upbitResult[0]['prev_closing_price']  # 전일종가
+            trade_volume = upbitResult[0]['trade_volume']  # 최근거래량
+            signed_change_rate = upbitResult[0]['signed_change_rate']  # 부호가있는변화율
+
+            # 슬롯에 코인정보 보내기
+            self.coinDataSent.emit(float(trade_price),
+                                   float(acc_trade_volume_24h),
+                                   float(acc_trade_price_24h),
+                                   float(high_price),
+                                   float(low_price),
+                                   float(prev_closing_price),
+                                   float(trade_volume),
+                                   float(signed_change_rate))
+
+            time.sleep(1) # api 호출 딜레이(1초마다 업비트 정보 호출)
+
+    def close(self):
+        self.alive = False
 
 
 class MainWindow(QMainWindow, form_class):
@@ -25,6 +58,7 @@ class MainWindow(QMainWindow, form_class):
         self.setWindowTitle("BitCoin Price Overview")
         self.setWindowIcon(QIcon("icons/bitcoin.png"))
         self.statusBar().showMessage('ver 0.5')
+
 
 
 
